ba7a8178495cac3f00d0bd71a0c6f4708ab4beae
masterteerat <teerat.saetan@gmail.com>
2023-01-08 16:24:47 +0700

    Try timer on switch activity

--- app/src/main/java/com/example/schoolproject/Thirdactivity.kt
+++ app/src/main/java/com/example/schoolproject/Thirdactivity.kt
@@ -3,13 +3,21 @@ package com.example.schoolproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.delay
+import android.os.Handler
+
 
 class Thirdactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thirdactivity)
-        val intent = Intent(this,Secondactivity::class.java)
-        startActivity(intent)
+        Handler().postDelayed({
+            val intent = Intent(this,Secondactivity::class.java)
+            startActivity(intent)
+        }, 2000)
+
     }
 }
+
+
+
+
