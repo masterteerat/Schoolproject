8a72af75debb7b7e81009428f8bcb5c95e1375fa
masterteerat <teerat.saetan@gmail.com>
2023-01-08 16:16:37 +0700

    Try add activity switch

--- app/src/main/java/com/example/schoolproject/Thirdactivity.kt
+++ app/src/main/java/com/example/schoolproject/Thirdactivity.kt
@@ -1,5 +1,6 @@
 package com.example.schoolproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.coroutines.delay
@@ -8,5 +9,7 @@ class Thirdactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thirdactivity)
+        val intent = Intent(this,Secondactivity::class.java)
+        startActivity(intent)
     }
 }
