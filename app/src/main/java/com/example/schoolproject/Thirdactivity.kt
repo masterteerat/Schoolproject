89359b079f02b0bc4587f5e4bf72a644e038314c
masterteerat <teerat.saetan@gmail.com>
2023-01-08 20:04:05 +0700

    Try checking file and switch activity depend on file existence

--- app/src/main/java/com/example/schoolproject/Thirdactivity.kt
+++ app/src/main/java/com/example/schoolproject/Thirdactivity.kt
@@ -4,17 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import java.io.File
 
 
 class Thirdactivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_thirdactivity)
-        Handler().postDelayed({
-            val intent = Intent(this,Secondactivity::class.java)
-            startActivity(intent)
-        }, 2000)
 
+        Handler().postDelayed({
+
+            val fileName = "data.txt"
+            var file = File(fileName)
+            var fileExists = file.exists()
+
+            if (fileExists) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                val intent = Intent(this, Secondactivity::class.java)
+                startActivity(intent)
+            }
+        }, 2000)
     }
 }
 
