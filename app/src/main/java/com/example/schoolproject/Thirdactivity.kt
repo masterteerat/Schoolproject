79c31fa262afbffb1f5e09c228eb3f9876f5f288
masterteerat <teerat.saetan@gmail.com>
2023-01-08 16:08:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/schoolproject/Thirdactivity.kt
@@ -0,0 +1,12 @@
+package com.example.schoolproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.delay
+
+class Thirdactivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thirdactivity)
+    }
+}
